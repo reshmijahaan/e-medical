fb9fbfce6593f12db237c584340c501c10ae60fd
shakil <shakilahamed0607@gmail.com>
2023-11-19 17:04:28 +0530

    second review

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,10 @@
 import express from "express";
 import {
     registerController,
-    loginController
+    loginController,
+    testController
   } from "../controllers/authController.js";
+  // import { requireSignIn } from "../middlewares/authMiddleware.js";
 
 //router object
 const router = express.Router();
@@ -13,5 +15,8 @@ router.post("/register", registerController);
 
 //LOGIN || POST
 router.post("/login", loginController);
+
+// test routes
+router.get('/test',testController)
 
 export default router;
